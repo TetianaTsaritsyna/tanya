b15e3d4f688a962074b646e660cfa7fa56c3db1a
Tetiana <tsaritsyna84@gmail.com>
2022-02-08 15:10:15 +0200

    Homework_loop

---
+++ Homework_loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_loop
+{
+    internal class Homework
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 9;
+            int sum = 0;
+            do
+            {
+
+                Console.WriteLine(sum + a);
+                a++;
+            }
+            while (a < b);
+        }
+    }
+}
