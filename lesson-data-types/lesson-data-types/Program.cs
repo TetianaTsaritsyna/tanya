2be029a546fe4f4a34db2b46b189ee28878f4910
Tetiana <tsaritsyna84@gmail.com>
2022-02-08 14:44:30 +0200

    Homework Data types

---
+++ lesson-data-types/lesson-data-types/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson_data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+         
+
+            int x = 5;
+            Console.WriteLine(-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15);
+
+            Console.WriteLine(Math.Abs(x) * Math.Sin(x));
+
+           
+            Console.WriteLine(2 * Math.PI * x);
+            Console.WriteLine(Math.Max(a, b));
+
+       
+        }
+    }
+}
