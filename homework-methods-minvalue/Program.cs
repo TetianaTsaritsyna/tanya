c4cf30cda27fcce47e20a21e7a38e9ce08891781
Tetiana <tsaritsyna84@gmail.com>
2022-02-10 16:43:12 +0200

    Homework - methods

---
+++ homework-methods-minvalue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homework_methods_minvalue
+{
+    internal class Program
+    {
+        static void MinimumValue(int a, int b)
+        {
+            if (a < b)
+            {
+                Console.WriteLine("Minimum is a");
+            }
+            else
+            {
+                Console.WriteLine("Minimum is a");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MaximumValue(10, 20);
+
+        }
+    }
+}
---
+++ homework-methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace homework_methods
+{
+     class Program
+    {
+        static int MaximumValue(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static double MaximumValue(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static void MinimumValue(int a, int b)
+        {
+            if (a < b)
+            {
+                Console.WriteLine("Minimum is a");
+            }
+            else
+            {
+                Console.WriteLine("Minimum is b");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //MaximumValue(13.9, 5.7);
+            MinimumValue(10, 25);
+            int myNum1 = MaximumValue(8, 5);
+            double myNum2 = MaximumValue(18.2, 5.9);
+            Console.WriteLine("Max: " + myNum1);
+            Console.WriteLine("Max: " + myNum2);
+
+
+        }
+    }
+}
+
